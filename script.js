f3d6bf152ec663c89fcb68ac6bf6e16b215b2b4b
LemboOFF <waffl3gaming7@gmail.com>
2026-06-08 15:19:38 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("goodButton").addEventListener("click", () => {
+    alert("YAY! YOU PRESSED THE GOOD BUTTON! YOU'RE AWESOME!");
+});
+
+document.getElementById("evilButton").addEventListener("click", () => {
+    alert("I TOLD U NOT TO PRESS IT WE'RE DOOMED!");
+});
+
+document.getElementById("breadImage").addEventListener("click", () => {
+    alert("mmm yum, hope it ain't soggy!");
+});
+
+document.getElementById("knifeImage").addEventListener("click", () => {
+    alert("u clicked the knife image! be careful with 'em, they can be dangerous!");
+});
+
+document.getElementById("chickenLegImage").addEventListener("click", () => {
+    alert("chicken wing chicken wing, hotdog and baloney!");
+});
